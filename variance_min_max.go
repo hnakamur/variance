9cace7ca2f57bab222cd88d6415bd80b5f0b9f41
Hiroaki Nakamura <hnakamur@gmail.com>
2019-08-09 15:52:32 +0900

    Stop embedding struct

--- variance_min_max.go
+++ variance_min_max.go
@@ -17,7 +17,7 @@ import "math"
 // responsibility to lock VarianceFloat64 appropriately
 // if it is accessed from multiple goroutines.
 type VarianceMinMaxFloat64 struct {
-	VarianceFloat64
+	v   VarianceFloat64
 	min float64
 	max float64
 }
@@ -35,13 +35,38 @@ func NewVarianceMinMaxFloat64() VarianceMinMaxFloat64 {
 // the min, and the max in the v, m2 is the sum
 // of the squared distance from the mean.
 func (v *VarianceMinMaxFloat64) Update(value float64) {
-	(&v.VarianceFloat64).Update(value)
+	v.v.Update(value)
 	if value < v.min {
 		v.min = value
 	}
 	if value > v.max {
 		v.max = value
 	}
+}
+
+// Count returns the count of sample values,
+// that is count of Update method called.
+func (v *VarianceMinMaxFloat64) Count() int {
+	return v.v.Count()
+}
+
+// Mean returns the mean value.
+// It returns NaN if Update was not called.
+func (v *VarianceMinMaxFloat64) Mean() float64 {
+	return v.v.Mean()
+}
+
+// Variance returns the variance.
+// It returns NaN if Update was not called.
+func (v *VarianceMinMaxFloat64) Variance() float64 {
+	return v.v.Variance()
+}
+
+// SampleVariance returns the sample variance.
+// It returns NaN if sample count (= count of Update method called)
+// is less than two.
+func (v *VarianceMinMaxFloat64) SampleVariance() float64 {
+	return v.v.SampleVariance()
 }
 
 // Min returns the minimum value.
